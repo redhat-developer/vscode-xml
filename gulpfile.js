4c922abb817573bc7e1190141275b6f8def16059
Nikolas Komonen <nikolaskomonen@gmail.com>
2018-07-11 11:49:14 -0400

    Fixed path values
    Signed-off-by: Nikolas Komonen <nikolaskomonen@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 const gulp = require('gulp');
 const gulp_tslint = require('gulp-tslint');
 const cp = require('child_process');
-const server_dir = '../xml-ls';
+const server_dir = '../lsp4xml';
 
 gulp.task('tslint', () => {
     return gulp.src(['**/*.ts', '!**/*.d.ts', '!node_modules/**'])
@@ -10,11 +10,11 @@ gulp.task('tslint', () => {
       .pipe(gulp_tslint.report());
 });
 
-gulp.task('build_server', ()=>
-{
-	cp.execSync(gradlew()+ ' shadowJar', {cwd:server_dir, stdio:[0,1,2]} );
-  gulp.src(server_dir +'/build/libs/xml-ls-1.0-SNAPSHOT-all.jar')
-		.pipe(gulp.dest('./server'))
+gulp.task('build_server', function(done) {
+	cp.execSync(mvnw()+ " clean verify", {cwd:server_dir, stdio:[0,1,2]} );
+  gulp.src(server_dir +'/org.eclipse.lsp4xml/target/org.eclipse.lsp4xml-all.jar')
+		.pipe(gulp.dest('./server'));
+	done();
 });
 
 
@@ -30,7 +30,6 @@ function isLinux() {
 	return /^linux/.test(process.platform);
 }
 
-function gradlew() {
-  // return server_dir +"/gradles";
-	return isWin()? server_dir+"/gradlew.cmd": server_dir+"/gradlew";
+function mvnw() {
+	return isWin()? server_dir+"/mvnw.cmd": server_dir+"/mvnw";
 }
