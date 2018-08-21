c70ef10ec1fb28aa05bcfa0bc9f88f9323d7740d
Nikolas Komonen <nikolaskomonen@gmail.com>
2018-08-21 14:52:57 -0400

    Intercepts didChangeConfiguration, added formatting preferences
    Signed-off-by: Nikolas Komonen <nikolaskomonen@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,24 @@
 
-import * as vscode from "vscode";
 import { prepareExecutable } from './javaServerStarter';
-import { LanguageClientOptions, RevealOutputChannelOn, LanguageClient } from 'vscode-languageclient';
+import { LanguageClientOptions, RevealOutputChannelOn, LanguageClient, DidChangeConfigurationNotification } from 'vscode-languageclient';
 import * as requirements from './requirements';
-import { window, commands, ExtensionContext } from "vscode";
+import { workspace, window, commands, ExtensionContext } from "vscode";
 import * as path from 'path';
+import * as os from 'os';
+
+interface Settings {
+  catalogs: String[],
+  logs: {},
+  format: {}
+}
 
 export function activate(context: ExtensionContext) {
   let storagePath = context.storagePath;
   if (!storagePath) {
-    storagePath = "~/lsp4xml/";
+    storagePath = os.homedir() + "/.lsp4xml";
   }
   let logfile = path.resolve(storagePath + '/lsp4xml.log');
+
   return requirements.resolveRequirements().catch(error => {
     //show error
     window.showErrorMessage(error.message, error.label).then((selection) => {
@@ -26,11 +33,18 @@ export function activate(context: ExtensionContext) {
       // Register the server for java
       documentSelector: ['xml'],
       revealOutputChannelOn: RevealOutputChannelOn.Never,
-      initializationOptions: {"lsp4xml.logPath": logfile}
-      
+      initializationOptions: {settings: getSettings() },
+      synchronize: {
+        configurationSection: ['xml']
+      },
+      middleware: {
+        workspace: {
+          didChangeConfiguration: () => languageClient.sendNotification(DidChangeConfigurationNotification.type, { settings: getSettings() })
+        }
+      }
+
     }
-    
-    
+
     let serverOptions = prepareExecutable(requirements);
     let languageClient = new LanguageClient('xml', 'XML Support', serverOptions, clientOptions);
     languageClient.onReady().then(() => {
@@ -38,7 +52,28 @@ export function activate(context: ExtensionContext) {
     });
     let disposable = languageClient.start();
     context.subscriptions.push(disposable);
-    
+
   });
+
+  function getSettings(): Settings {
+
+    let configXML = workspace.getConfiguration('xml');
+    let configCatalogs = configXML.get('catalogs') as String[];
+    let configFormats = configXML.get('format');
+
+    let configLogs = configXML.get('logs');
+    configLogs["file"] = logfile;
+
+    let settings: Settings = {
+      catalogs: configCatalogs,
+      logs: configLogs,
+      format: configFormats
+    }
+
+    return settings;
+  }
+
+
+
 
 }
