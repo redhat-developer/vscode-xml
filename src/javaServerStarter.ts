164965a44cabf58a8e4caa31ba2996d23c588388
Nikolas Komonen <nikolaskomonen@gmail.com>
2018-09-12 11:25:14 -0400

    extensions.ts fetches/sends capabilities and package.json updated with serverCapabilities
    Signed-off-by: Nikolas Komonen <nikolaskomonen@gmail.com>

--- src/javaServerStarter.ts
+++ src/javaServerStarter.ts
@@ -8,12 +8,14 @@ import * as net from 'net';
 const glob = require('glob');
 
 declare var v8debug;
+
 const DEBUG = (typeof v8debug === 'object') || startedInDebugMode();
 
 export function prepareExecutable(requirements: RequirementsData): Executable {
 	let executable: Executable = Object.create(null);
 	let options: ExecutableOptions = Object.create(null);
 	options.env = process.env;
+	
 	options.stdio = 'pipe';
 	executable.options = options;
 	executable.command = path.resolve(requirements.java_home + '/bin/java');
@@ -26,10 +28,9 @@ function prepareParams(requirements: RequirementsData): string[] {
 	if (DEBUG) {
 		params.push('-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1054,quiet=y');
 		// suspend=y is the default. Use this form if you need to debug the server startup code:
-		//  params.push('-agentlib:jdwp=transport=dt_socket,server=y,address=1054');
+		//params.push('-agentlib:jdwp=transport=dt_socket,server=y,address=1054');
 	}
 	let server_home: string = path.resolve(__dirname, '../../server');
-	params.push('-jar');
 	let launchersFound: Array<string> = glob.sync('**/org.eclipse.lsp4xml-all.jar', { cwd: server_home });
 	if (launchersFound.length) {
 		params.push('-jar'); params.push(path.resolve(server_home, launchersFound[0]));
