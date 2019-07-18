b90f57dc0075389c80cc43224128d784aca96fcd
David Kwon <dakwon@redhat.com>
2019-07-18 16:08:45 -0400

    Fix unexpected indentation after empty element
    Signed-off-by: David Kwon <dakwon@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -243,7 +243,7 @@ function getIndentationRules(): LanguageConfiguration {
     // indentationRules referenced from:
     // https://github.com/microsoft/vscode/blob/d00558037359acceea329e718036c19625f91a1a/extensions/html-language-features/client/src/htmlMain.ts#L114-L115
     indentationRules: {
-      increaseIndentPattern: /<([-_\.A-Za-z0-9]+)(?=\s|>)\b[^>]*>(?!.*<\/\1>)|<!--(?!.*-->)|\{[^}"']*$/,
+      increaseIndentPattern: /<(?!\?|[^>]*\/>)([-_\.A-Za-z0-9]+)(?=\s|>)\b[^>]*>(?!.*<\/\1>)|<!--(?!.*-->)|\{[^}"']*$/,
       decreaseIndentPattern: /^\s*(<\/[-_\.A-Za-z0-9]+\b[^>]*>|-->|\})/
     },
     onEnterRules: [
@@ -256,7 +256,7 @@ function getIndentationRules(): LanguageConfiguration {
         beforeText: new RegExp(`<(\\w[\\w\\d]*)([^/>]*(?!/)>)[^<]*$`, 'i'),
         action: { indentAction: IndentAction.Indent }
       }
-    ],
+    ]
   };
 }
 
