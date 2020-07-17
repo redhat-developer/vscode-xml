d11fc9e1cab46a7a2b191264664f3cfd949954e8
Ryan Zegray <ryan.zegray@ibm.com>
2020-07-17 13:48:52 -0400

    Dispose extensions did change listener
    Signed-off-by: Ryan Zegray <ryan.zegray@ibm.com>

--- src/extension.ts
+++ src/extension.ts
@@ -138,9 +138,9 @@ export function activate(context: ExtensionContext) {
       context.subscriptions.push(activateTagClosing(tagProvider, { xml: true, xsl: true }, Commands.AUTO_CLOSE_TAGS));
 
       if (extensions.onDidChange) {// Theia doesn't support this API yet
-        extensions.onDidChange(() => {
+        context.subscriptions.push(extensions.onDidChange(() => {
           onExtensionChange(extensions.all);
-        });
+        }));
       }
 
     });
