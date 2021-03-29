621b11a8b317ef74c3aca9af54202598308132cf
Roland Grunberg <rgrunber@redhat.com>
2021-03-29 13:49:02 -0400

    Fix license label to reflect EPL v2.0
    Signed-off-by: Roland Grunberg <rgrunber@redhat.com>

--- src/commands/commandConstants.ts
+++ src/commands/commandConstants.ts
@@ -1,7 +1,7 @@
 /**
  *  Copyright (c) 2019 Red Hat, Inc. and others.
  *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
+ *  are made available under the terms of the Eclipse Public License v2.0
  *  which accompanies this distribution, and is available at
  *  https://www.eclipse.org/legal/epl-v20.html
  *
