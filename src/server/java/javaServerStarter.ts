6ea1224613085249b0cda9038528f59b5d408e73
David Thompson <davthomp@redhat.com>
2021-04-21 09:29:50 -0400

    Revert "Update vscode-languageclient to 7.0.0"
    This reverts commit c248ffb6bd51ec9b459b07e1828d0e718a2e1b02.

--- src/server/java/javaServerStarter.ts
+++ src/server/java/javaServerStarter.ts
@@ -1,7 +1,7 @@
 import * as os from 'os';
 import * as path from 'path';
 import { ExtensionContext, workspace } from 'vscode';
-import { Executable } from 'vscode-languageclient/node';
+import { Executable } from 'vscode-languageclient';
 import { RequirementsData } from '../requirements';
 import { getJavaagentFlag, getKey, getXMLConfiguration, IS_WORKSPACE_VMARGS_XML_ALLOWED, xmlServerVmargs } from '../../settings/settings';
 import { getProxySettings, getProxySettingsAsJVMArgs, ProxySettings, jvmArgsContainsProxySettings } from '../../settings/proxySettings';
