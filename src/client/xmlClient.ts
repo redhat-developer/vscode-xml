dc5383495eec3b51794771819a65ab13a7c74d40
Alexander Chen <alchen@redhat.com>
2021-07-23 15:34:31 -0400

    set bindingWizardSupport for client support

--- src/client/xmlClient.ts
+++ src/client/xmlClient.ts
@@ -130,6 +130,7 @@ function getLanguageClientOptions(logfile: string, externalXmlSettings: External
         },
         actionableNotificationSupport: true,
         openSettingsCommandSupport: true,
+        bindingWizardSupport: true,
         shouldLanguageServerExitOnShutdown: true
       }
     },
