48cedaf3123ff614277e9e5b702c8fbbd2503cde
Roland Grunberg <rgrunber@redhat.com>
2022-06-26 19:39:55 -0400

    Add support for pre-releases.
    - VS Code Engine to 1.63.0
    - Upgrade build jobs to use NodeJS 14
    - Add prepare_pre_release gulp task
    - Use the osx-x86_64 binary for darwin-arm64
    
    Signed-off-by: Roland Grunberg <rgrunber@redhat.com>

--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,22 @@ gulp.task('build_server_with_binary', function(done) {
 	done();
 });
 
+gulp.task('prepare_pre_release', function(done) {
+    const json = JSON.parse(fse.readFileSync("./package.json").toString());
+    const stableVersion = json.version.match(/(\d+)\.(\d+)\.(\d+)/);
+    const major = stableVersion[1];
+    const minor = stableVersion[2];
+    const date = new Date();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours();
+    const patch = `${date.getFullYear()}${prependZero(month)}${prependZero(day)}${prependZero(hours)}`;
+    const insiderPackageJson = Object.assign(json, {
+        version: `${major}.${minor}.${patch}`,
+    });
+    fse.writeFileSync("./package.json", JSON.stringify(insiderPackageJson, null, 2));
+    done();
+});
 
 function isWin() {
 	return /^win/.test(process.platform);
@@ -49,4 +65,11 @@ function isLinux() {
 
 function mvnw() {
 	return isWin()? "mvnw.cmd": server_dir+"/mvnw";
+}
+
+function prependZero(number) {
+    if (number > 99) {
+        throw "Unexpected value to prepend with zero";
+    }
+    return `${number < 10 ? "0" : ""}${number}`;
 }
