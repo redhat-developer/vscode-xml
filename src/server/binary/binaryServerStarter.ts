3bf70704ee846d9aea0737f2114e16b4e2cb9c02
Ciro Lo Sapio <lscorionjs@gmail.com>
2024-07-18 00:30:49 +0200

    chore: refactor

--- src/server/binary/binaryServerStarter.ts
+++ src/server/binary/binaryServerStarter.ts
@@ -213,7 +213,7 @@ function getBinaryEnvironment(): any {
  *
  * @returns The platform of the current operating system.
  */
-function getPlatform (): ReturnType<typeof os.platform> | 'alpine' {
+function getPlatform (): NodeJS.Platform | 'alpine' {
   if (fs.existsSync('/etc/alpine-release')) return 'alpine'
   return os.platform()
 }
