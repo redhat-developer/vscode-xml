6875f5064085a8f67ec73bf47941a8ecaf1f4c3e
azerr <azerr@redhat.com>
2024-12-11 15:23:56 +0100

    Fix crash of extension when proxy settings address has wrong format
    Signed-off-by: azerr <azerr@redhat.com>

--- src/settings/proxySettings.ts
+++ src/settings/proxySettings.ts
@@ -75,7 +75,7 @@ export function getProxySettings(): ProxySettings {
     return null;
   }
   const regexResult = HOST_AND_PORT_EXTRACTOR.exec(proxyAddress);
-  if (!regexResult[1]) {
+  if (!regexResult || !regexResult[1]) {
     return null;
   }
   const host: string = regexResult[1];
